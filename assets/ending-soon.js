aa40e7b9e810737c4e751f8b1af9c5b058615848
German Rosario <ray_jr@hotmail.es>
2024-05-11 16:55:50 -0400

    feat: add ending soon feature to high bids section

---
+++ assets/ending-soon.js
@@ -0,0 +1,28 @@
+async function getSections() {
+    const sections = [
+        "template--22516178452777__product-grid",
+    ];
+    const response = await fetch(`${window.ending_soon_url}&sections=${sections.join(",")}`);
+    const content = await response.json();
+
+    return content["template--22516178452777__product-grid"];
+}
+
+function addToWrapper(wrapper, children) {
+    for (let index = 0; index < children.length; index++) {
+        const item = children[index];
+
+        wrapper.children[index].innerHTML = item.innerHTML;
+    }
+}
+
+document.addEventListener("DOMContentLoaded", async function() {
+    const content = await getSections();
+    const wrapper = document.getElementById('Slider-template--22516178321705__d2543e03-de3f-44ac-9b18-349e4b4e3117');
+    const html = new DOMParser().parseFromString(content, 'text/html');
+    const children = html?.querySelectorAll("#product-grid li");
+    const first10 = Array.from(children).slice(0, 10);
+
+
+    addToWrapper(wrapper, first10);
+});
